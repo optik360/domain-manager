5402ad916d8839d86f44673f13c01087e3a81ca0
vigpatrick <patrickk.vvigneault@gmail.com>
2016-06-08 15:16:39 +0000

    Remove useless file

--- testConnectionHttp.rb
+++
@@ -1,7 +0,0 @@
-require 'godaddy/api'
-
-api = Godaddy::Api.new('VUqpsHVZ_3v7CikmVW4HB2EgxqFtCSz', '3v7GxU3QdmAyL9GSCPmav2')
-
-result = api.get('/v1/domains/available?domain=www.gooogle.ca')
-
-puts result
